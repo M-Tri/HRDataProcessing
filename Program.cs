9e412574ec43e82705d60903471aa9ef0b1afe70
MT <69726672+M-Tri@users.noreply.github.com>
2025-02-16 11:04:15 -0700

    Add basic project files (README, .gitignore, Program.cs, data.csv...)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System ;
+
+namespace dotnetProject
+{
+    class Program {
+        static void Main(String[] args){
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
