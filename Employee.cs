6d3127bbbe2c40689ceb5f20151f0596d95e9886
MT <69726672+M-Tri@users.noreply.github.com>
2025-02-16 12:35:01 -0700

    Add a class : Employee
    - Add getters/setters for the following columns:
      'EmployeeID', 'StartDate', 'Title', 'BusinessUnit', 'EmployeeStatus', 'EmployeeType',
      'PayZone', 'EmployeeClassificationType', 'DepartmentType', 'Division', 'DOB', 'State',
      'GenderCode', 'RaceDesc', 'MaritalDesc', 'Performance Score', 'Current Employee Rating',
      'Survey Date', 'Engagement Score', 'Satisfaction Score', 'Work-Life Balance Score',
      'Training Date', 'Training Program Name', 'Training Type', 'Training Outcome',
      'Training Duration(Days)', 'Training Cost', 'Age'.

---
+++ Employee.cs
@@ -0,0 +1,32 @@
+public class Employee
+//Assumptions: Each property must always have a value. They are Non-Nullable
+{
+    public int EmployeeID { get; set; }
+    public string StartDate { get; set; }
+    public string Title { get; set; }
+    public string BusinessUnit { get; set; }
+    public string EmployeeStatus { get; set; }
+    public string EmployeeType { get; set; }
+    public string PayZone { get; set; }
+    public string EmployeeClassificationType { get; set; }
+    public string DepartmentType { get; set; }
+    public string Division { get; set; }
+    public string DOB { get; set; }
+    public string State { get; set; }
+    public string GenderCode { get; set; }
+    public string RaceDesc { get; set; }
+    public string MaritalDesc { get; set; }
+    public string PerformanceScore { get; set; }
+    public int CurrentEmployeeRating { get; set; }
+    public string SurveyDate { get; set; }
+    public int EngagementScore { get; set; }
+    public int SatisfactionScore { get; set; }
+    public int WorkLifeBalanceScore { get; set; }
+    public string TrainingDate { get; set; }
+    public string TrainingProgramName { get; set; }
+    public string TrainingType { get; set; }
+    public string TrainingOutcome { get; set; }
+    public int TrainingDurationDays { get; set; }
+    public double TrainingCost { get; set; }
+    public int Age { get; set; }
+}
